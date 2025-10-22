4c1169f414a984d7bd6e25766d2ec214355ac68f
Rui Costa <rui.costa@inovador.net>
2025-10-22 23:49:38 +0100

    Fix route parameter

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $featuredFuneralHomes = \App\Models\FuneralHome::query()
         ->with(['images', 'categories'])
-        ->where('is_accepted', true)
         ->inRandomOrder()
         ->limit(9)
         ->get();
